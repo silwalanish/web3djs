15fdb839ec70d7e1598e8e52f961d0641b434d0b
silwalanish <silwalanish@gmail.com>
2019-11-24 18:03:09 +0545

    Add abstract shader class

---
+++ js/src/core/rendering/shader.ts
@@ -0,0 +1,26 @@
+import { genShaderProgram, ShaderProgram } from './shader.utils';
+
+export interface ShaderSource {
+  vertex: string;
+  fragment: string;
+}
+
+export default class Shader {
+  private readonly shaderProgram: ShaderProgram;
+
+  protected constructor(gl: WebGLRenderingContext, source: ShaderSource) {
+    this.shaderProgram = genShaderProgram(gl, source.vertex, source.fragment);
+  }
+
+  public use(gl: WebGLRenderingContext) : void {
+    gl.useProgram(this.shaderProgram);
+  }
+
+  protected getAttribLocation(gl: WebGLRenderingContext, attribName: string) : GLuint | null {
+    return this.shaderProgram && gl.getAttribLocation(this.shaderProgram, attribName);
+  }
+
+  protected getUniformLocation(gl: WebGLRenderingContext, uniformName: string) : WebGLUniformLocation | null {
+    return this.shaderProgram && gl.getUniformLocation(this.shaderProgram, uniformName);
+  }
+}
