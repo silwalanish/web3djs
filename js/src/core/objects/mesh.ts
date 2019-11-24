c4a2ce7ec5fdebf9d3fe31f2d201dd728452b08f
silwalanish <silwalanish@gmail.com>
2019-11-24 22:41:41 +0545

    Add optional color buffers to mesh

--- js/src/core/objects/mesh.ts
+++ js/src/core/objects/mesh.ts
@@ -1,33 +1,49 @@
 import Shader from '../rendering/shader';
 import { Buffer, createFloatBuffer } from '../utils/buffer.utils';
 
-export interface MeshMeta{
-  numVertex: number,
-  firstVertex: number,
-  meshType: GLuint
+export interface MeshMeta {
+  numVertex: number;
+  firstVertex: number;
+  meshType: GLuint;
 }
 
-export interface PositionMeta {
-  components: number,
-  normalize: boolean,
-  stride: number,
-  offset: number
+export interface BufferMeta {
+  components: number;
+  normalize: boolean;
+  stride: number;
+  offset: number;
 }
 
 export default class Mesh {
   private positionBuffer: Buffer;
-  private positionMeta: PositionMeta;
+  private positionMeta: BufferMeta;
+
+  private colorBuffer: Buffer;
+  private colorMeta: BufferMeta;
+
   private meshMeta: MeshMeta;
 
   public constructor(
     gl: WebGLRenderingContext,
     meta: MeshMeta,
     position: GLfloat[],
-    posMetaData: PositionMeta
+    posMetaData: BufferMeta,
+    color?: GLfloat[],
+    colorMetaData?: BufferMeta
   ) {
     this.meshMeta = meta;
     this.positionBuffer = createFloatBuffer(gl, position, gl.STATIC_DRAW);
     this.positionMeta = posMetaData;
+
+    if (color) {
+      this.colorBuffer = createFloatBuffer(gl, color, gl.STATIC_DRAW);
+      this.colorMeta = colorMetaData || {
+        components: 4,
+        normalize: false,
+        stride: 0,
+        offset: 0
+      };
+    }
   }
 
   public bind(gl: WebGLRenderingContext, shader: Shader): void {
@@ -38,8 +54,20 @@ export default class Mesh {
       gl.FLOAT,
       this.positionMeta.normalize,
       this.positionMeta.stride,
-      this.positionMeta.stride
+      this.positionMeta.offset
     );
+
+    if (this.colorBuffer) {
+      gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
+      shader.enableVertexColor(
+        gl,
+        this.colorMeta.components,
+        gl.FLOAT,
+        this.colorMeta.normalize,
+        this.colorMeta.stride,
+        this.colorMeta.offset
+      );
+    }
   }
 
   public render(gl: WebGLRenderingContext): void {
