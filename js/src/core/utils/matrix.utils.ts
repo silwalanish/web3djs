43d44ed64b237bc8f135b2f28b5a6056b3427b7b
silwalanish <silwalanish@gmail.com>
2019-12-07 15:12:32 +0545

    Add utils to calculate view and projection matrix

--- js/src/core/utils/matrix.utils.ts
+++ js/src/core/utils/matrix.utils.ts
@@ -1,6 +1,6 @@
 import { vec3, mat4 } from 'gl-matrix';
 
-function toRadians(deg: number): number {
+export function toRadians(deg: number): number {
   return deg * (Math.PI / 180);
 }
 
@@ -16,4 +16,24 @@ export function calculateModelMat(position: vec3, rotation: vec3, scale: vec3): 
   mat4.scale(modelMatrix, modelMatrix, scale);
 
   return modelMatrix;
+}
+
+export function calculateViewMat(eyePos: vec3, front: vec3, up: vec3): mat4 {
+  const viewMatrix: mat4 = mat4.create();
+
+  let center: vec3 = vec3.create();
+  
+  vec3.add(center, eyePos, front);
+  
+  mat4.lookAt(viewMatrix, eyePos, center, up);
+
+  return viewMatrix;
+}
+
+export function calculateProjectionMat(fieldOfView: number, aspect: number, zNear: number, zFar: number): mat4 {
+  const projectionMatrix: mat4 = mat4.create();
+
+  mat4.perspective(projectionMatrix, toRadians(fieldOfView), aspect, zNear, zFar);
+
+  return projectionMatrix;
 }
