cca319ac140f05d13266a4207c29e66b30f86705
silwalanish <silwalanish@gmail.com>
2019-12-07 11:56:51 +0545

    Change order to Translate, Rotate and Scale

--- js/src/core/utils/matrix.utils.ts
+++ js/src/core/utils/matrix.utils.ts
@@ -7,13 +7,13 @@ function toRadians(deg: number): number {
 export function calculateModelMat(position: vec3, rotation: vec3, scale: vec3): mat4 {
   const modelMatrix: mat4 = mat4.create();
 
-  mat4.scale(modelMatrix, modelMatrix, scale);
+  mat4.translate(modelMatrix, modelMatrix, position);
 
   mat4.rotateX(modelMatrix, modelMatrix, toRadians(rotation[0]));
   mat4.rotateY(modelMatrix, modelMatrix, toRadians(rotation[1]));
   mat4.rotateZ(modelMatrix, modelMatrix, toRadians(rotation[2]));
 
-  mat4.translate(modelMatrix, modelMatrix, position);
+  mat4.scale(modelMatrix, modelMatrix, scale);
 
   return modelMatrix;
 }
