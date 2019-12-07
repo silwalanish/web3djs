1d733df90aa0ad9c4b818ee8ef9ec9782307e253
silwalanish <silwalanish@gmail.com>
2019-12-07 16:49:00 +0545

    Implement methods to set vector uniforms and 1f/i uniforms

--- js/src/core/rendering/shader.ts
+++ js/src/core/rendering/shader.ts
@@ -1,3 +1,5 @@
+import { vec3 } from 'gl-matrix';
+import { Texture } from '../utils/texture.utils';
 import { genShaderProgram, ShaderProgramType } from '../utils/shader.utils';
 
 export interface ShaderSource {
@@ -24,16 +26,19 @@ export default abstract class Shader {
     return this.shaderProgram && gl.getUniformLocation(this.shaderProgram, uniformName);
   }
 
-  public abstract enableVertexPosition(
-    gl: WebGLRenderingContext,
-    components: number,
-    type: GLuint,
-    normalize: GLboolean,
-    stride: GLuint,
-    offset: GLuint
-  ): void;
+  public setUniform1i(gl: WebGLRenderingContext, loc: WebGLUniformLocation | null, val: number): void {
+    gl.uniform1i(loc, val);
+  }
 
-  public abstract enableIndex(
+  public setUniform1f(gl: WebGLRenderingContext, loc: WebGLUniformLocation | null, val: number): void {
+    gl.uniform1i(loc, val);
+  }
+
+  public setUniform3f(gl: WebGLRenderingContext, loc: WebGLUniformLocation | null, val: vec3): void {
+    gl.uniform3fv(loc, val);
+  }
+
+  public abstract enableVertexPosition(
     gl: WebGLRenderingContext,
     components: number,
     type: GLuint,
@@ -74,4 +79,6 @@ export default abstract class Shader {
   public abstract setViewMatrix(gl: WebGLRenderingContext, mat: Float32List): void;
 
   public abstract setModelMatrix(gl: WebGLRenderingContext, mat: Float32List): void;
+
+  public abstract setTexture(gl: WebGLRenderingContext, tex: Texture, texPos?: number): void;
 }
