64b1f16a2e94718c9034b2206a73743a7e563935
silwalanish <silwalanish@gmail.com>
2019-12-08 21:16:19 +0545

    Update demo to show monkey and boxes

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,324 +1,195 @@
-import { mat4, vec3 } from 'gl-matrix';
+import { vec3, vec2 } from 'gl-matrix';
+import * as OBJLoader from 'webgl-obj-loader';
 
+import Cube from './game/cube';
+import Plane from './game/plane';
 import { info } from './core/logger';
 import Mesh from './core/objects/mesh';
-import DefaultShader from './core/rendering/shader.default';
+import Camera from './core/rendering/camera';
 import GameObject from './core/objects/object';
+import DefaultShader from './core/rendering/shader.default';
+import TextureShader from './core/rendering/shader.texture';
+import { Texture, genTexture2D } from './core/utils/texture.utils';
 
 const canvas: HTMLCanvasElement = document.createElement('canvas');
 
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = window.outerWidth;
+canvas.height = window.outerHeight;
 
 const gl: WebGLRenderingContext | null = canvas.getContext('webgl');
 
-if (gl) {
-  document.body.appendChild(canvas);
-  const cubePositions = [
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    1,
-    -1,
-    1,
-    1,
-    1,
-    1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    1,
-    -1,
-    1,
-    -1,
-    1,
-    -1,
-    -1,
-    -1,
-    1,
-    -1,
-    -1,
-    1,
-    1,
-    -1,
-    1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    -1,
-    1,
-    1,
-    -1,
-    1,
-    -1,
-    1,
-    -1,
-    1,
-    -1,
-    -1,
-    1,
-    -1,
-    -1,
-    -1,
-    -1,
-    1,
-    1,
-    -1,
-    -1,
-    1,
-    1,
-    -1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    -1,
-    -1,
-    1,
-    1,
-    -1,
-    1,
-    -1,
-    -1,
-    1,
-    1,
-    1,
-    1,
-    -1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    -1,
-    -1,
-    1,
-    -1,
-    1,
-    1,
-    1,
-    -1,
-    1,
-    -1,
-    -1,
-    1,
-    1,
-    1,
-    1,
-    1,
-    -1,
-    1,
-    1,
-    1,
-    -1,
-    1
-  ];
+let mouse = {
+  down: false,
+  first: true,
+  sensitivity: 0.05,
+  position: vec2.fromValues(0, 0),
+  oldPosition: vec2.fromValues(0, 0),
+  onMouseDown(e) {
+    this.down = true;
+    this.oldPosition = vec2.clone(this.position);
+    this.position = vec2.fromValues(e.clientX, e.clientY);
+  },
+  onMouseUp(e) {
+    this.down = false;
+    this.oldPosition = vec2.clone(this.position);
+    this.position = vec2.fromValues(e.clientX, e.clientY);
+  },
+  onMouseMove(e: MouseEvent, camera: Camera) {
+    this.oldPosition = vec2.clone(this.position);
+    this.position = vec2.fromValues(e.clientX, e.clientY);
 
-  const faceColors = [
-    0.583,
-    0.771,
-    0.014,
-    0.609,
-    0.115,
-    0.436,
-    0.327,
-    0.483,
-    0.844,
-    0.822,
-    0.569,
-    0.201,
-    0.435,
-    0.602,
-    0.223,
-    0.31,
-    0.747,
-    0.185,
-    0.597,
-    0.77,
-    0.761,
-    0.559,
-    0.436,
-    0.73,
-    0.359,
-    0.583,
-    0.152,
-    0.483,
-    0.596,
-    0.789,
-    0.559,
-    0.861,
-    0.639,
-    0.195,
-    0.548,
-    0.859,
-    0.014,
-    0.184,
-    0.576,
-    0.771,
-    0.328,
-    0.97,
-    0.406,
-    0.615,
-    0.116,
-    0.676,
-    0.977,
-    0.133,
-    0.971,
-    0.572,
-    0.833,
-    0.14,
-    0.616,
-    0.489,
-    0.997,
-    0.513,
-    0.064,
-    0.945,
-    0.719,
-    0.592,
-    0.543,
-    0.021,
-    0.978,
-    0.279,
-    0.317,
-    0.505,
-    0.167,
-    0.62,
-    0.077,
-    0.347,
-    0.857,
-    0.137,
-    0.055,
-    0.953,
-    0.042,
-    0.714,
-    0.505,
-    0.345,
-    0.783,
-    0.29,
-    0.734,
-    0.722,
-    0.645,
-    0.174,
-    0.302,
-    0.455,
-    0.848,
-    0.225,
-    0.587,
-    0.04,
-    0.517,
-    0.713,
-    0.338,
-    0.053,
-    0.959,
-    0.12,
-    0.393,
-    0.621,
-    0.362,
-    0.673,
-    0.211,
-    0.457,
-    0.82,
-    0.883,
-    0.371,
-    0.982,
-    0.099,
-    0.879
-  ];
+    if (this.down && !this.frist) {
+      let delta: vec2 = vec2.create();
+      vec2.sub(delta, this.position, this.oldPosition);
+      vec2.scale(delta, delta, this.sensitivity);
 
-  const cubeMesh: Mesh = new Mesh(
-    gl,
-    { numVertex: 4 * 6 * 2, firstVertex: 0, meshType: gl.TRIANGLES },
-    cubePositions,
-    {
-      components: 3,
-      normalize: false,
-      stride: 0,
-      offset: 0
-    },
-    faceColors,
-    {
-      components: 3,
-      normalize: false,
-      stride: 0,
-      offset: 0
+      camera.rotation[0] += -delta[0];
+      camera.rotation[1] += delta[1];
+
+      if (camera.rotation[1] > 89.0) camera.rotation[1] = 89.0;
+      if (camera.rotation[1] < -89.0) camera.rotation[1] = -89.0;
+
+      camera.calculateFront();
     }
-  );
 
-  const positions: GLfloat[] = [-2.0, 2.0, 2.0, 2.0, -2.0, -2.0, 2.0, -2.0];
-  const colors: GLfloat[] = [1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0];
+    this.first = false;
+  }
+};
 
-  const planeMesh: Mesh = new Mesh(
-    gl,
-    {
-      numVertex: 4,
-      firstVertex: 0,
-      meshType: gl.TRIANGLE_STRIP
-    },
-    positions,
-    {
-      components: 2,
-      normalize: false,
-      stride: 0,
-      offset: 0
-    },
-    colors
-  );
-
-  const fieldOfView = (60 * Math.PI) / 180;
-  const aspect = gl.canvas.width / gl.canvas.height;
-  const zNear = 0.1;
-  const zFar = 100.0;
-  const projectionMatrix = mat4.create();
-
-  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
-
-  const viewMatrix = mat4.create();
-  const eyePos = vec3.fromValues(0, 0, 6);
-  const center = vec3.create();
-  const up = vec3.fromValues(0, 1, 0);
-
-  const plane1: GameObject = new GameObject(vec3.fromValues(2, 0, -2), vec3.fromValues(0, 0, 0), planeMesh);
-  const plane2: GameObject = new GameObject(vec3.fromValues(-2, 0, -2), vec3.fromValues(0, 0, 0), planeMesh);
-
-  const cube: GameObject = new GameObject(vec3.fromValues(0, 0, 0), vec3.create(), cubeMesh);
-
-  let shader: DefaultShader = new DefaultShader(gl);
-
-  window.requestAnimationFrame(function draw() {
-    mat4.lookAt(viewMatrix, eyePos, center, up);
-
-    gl.clearColor(0, 0, 0, 1);
-    gl.clearDepth(1);
-
-    gl.enable(gl.DEPTH_TEST);
-    gl.depthFunc(gl.LEQUAL);
-
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    shader.use(gl);
-    shader.setProjectionMatrix(gl, projectionMatrix);
-    shader.setViewMatrix(gl, viewMatrix);
-
-    plane1.render(gl, shader);
-    plane2.render(gl, shader);
-
-    cube.render(gl, shader);
-
-    cube.rotation[1] += 1;
-
-    window.requestAnimationFrame(draw);
-  });
-} else {
-  info("The browser doesn't support WebGL.");
+function keyDown(e: KeyboardEvent, camera: Camera) {
+  switch (e.keyCode) {
+    case 87:
+      camera.moveForward();
+      break;
+    case 83:
+      camera.moveBack();
+      break;
+    case 65:
+      camera.moveLeft();
+      break;
+    case 68:
+      camera.moveRight();
+      break;
+    case 90:
+      break;
+    case 88:
+      break;
+  }
 }
+
+function fullScreen() {
+  canvas.requestFullscreen();
+}
+
+interface DownloadedMeshes extends OBJLoader.MeshMap {
+  monkeyObj: OBJLoader.Mesh;
+}
+
+function play(meshes: DownloadedMeshes) {
+  if (gl) {
+    document.body.appendChild(canvas);
+
+    canvas.addEventListener('dblclick', fullScreen);
+    canvas.addEventListener('mousedown', mouse.onMouseDown.bind(mouse));
+    canvas.addEventListener('mouseup', mouse.onMouseUp.bind(mouse));
+
+    Cube.createMesh(gl);
+    Plane.createMesh(gl);
+
+    const camera = new Camera({
+      fov: 60,
+      aspect: gl.canvas.width / gl.canvas.height,
+      zNear: 0.01,
+      zFar: 1000.0,
+      position: vec3.fromValues(0, 4, 4)
+    });
+
+    document.addEventListener('keydown', e => keyDown(e, camera));
+    canvas.addEventListener('mousemove', e => mouse.onMouseMove(e, camera));
+
+    let cubes: Cube[] = [];
+
+    for (let i = 0; i < 7; i++) {
+      for (let j = 0; j < 7; j++) {
+        cubes.push(
+          new Cube({
+            position: vec3.fromValues(-15 + i * 5, 0, -15 + j * 5),
+            scale: vec3.fromValues(Math.max(1, Math.random() * 2), 1, Math.max(1, Math.random() * 2))
+          })
+        );
+      }
+    }
+
+    const { monkeyObj } = meshes;
+
+    let monkeyMesh: Mesh = new Mesh({
+      gl,
+      meta: {
+        firstVertex: 0,
+        meshType: gl.TRIANGLES,
+        numVertex: monkeyObj.indices.length
+      },
+      positions: monkeyObj.vertices,
+      uvs: monkeyObj.textures,
+      indices: monkeyObj.indices,
+      posMetaData: {
+        components: 3,
+        normalize: false,
+        offset: 0,
+        stride: 0
+      }
+    });
+
+    let monkey: GameObject = new GameObject({
+      mesh: monkeyMesh,
+      position: vec3.fromValues(0, 3, -3)
+    });
+
+    let texShader: TextureShader = new TextureShader(gl);
+    let shader: DefaultShader = new DefaultShader(gl);
+
+    let boxTex: Texture = genTexture2D(gl, {
+      url: '/assets/RTS_Crate.png',
+      minFilter: gl.NEAREST
+    });
+
+    let monkeyTex: Texture = genTexture2D(gl, {
+      url: '/assets/monkey.png',
+      minFilter: gl.NEAREST
+    });
+
+    const wallColor: vec3 = vec3.fromValues(0.2, 0.4, 0.6);
+
+    window.requestAnimationFrame(function draw() {
+      camera.calculateMatrix();
+
+      gl.clearColor(0, 0, 0, 1);
+      gl.clearDepth(1);
+
+      gl.enable(gl.DEPTH_TEST);
+      gl.depthFunc(gl.LEQUAL);
+
+      gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+      texShader.use(gl);
+      texShader.setProjectionMatrix(gl, camera.getProjectionMatrix());
+      texShader.setViewMatrix(gl, camera.getViewMatrix());
+
+      texShader.setTexture(gl, boxTex);
+      for (let cube of cubes) {
+        cube.render(gl, texShader);
+      }
+
+      texShader.setTexture(gl, monkeyTex);
+      monkey.render(gl, texShader);
+
+      window.requestAnimationFrame(draw);
+    });
+  } else {
+    info("The browser doesn't support WebGL.");
+  }
+}
+
+window.onload = () => {
+  OBJLoader.downloadMeshes({ monkeyObj: '/assets/monkey.obj' }, play, {});
+};
