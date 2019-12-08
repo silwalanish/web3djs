0336878f2715a8a5a3cdd66f42de99ec3c093d3c
silwalanish <silwalanish@gmail.com>
2019-12-08 20:11:51 +0545

    Add texture size in texture shader

--- js/src/core/rendering/shader.texture.ts
+++ js/src/core/rendering/shader.texture.ts
@@ -24,9 +24,10 @@ const TEXTURE_SHADER_SOURCE: ShaderSource = {
     varying vec2 vUV;
 
     uniform sampler2D uTex;
+    uniform float uTexSize;
 
     void main() {
-      vec4 texColor = texture2D(uTex, vUV);
+      vec4 texColor = texture2D(uTex, vUV * uTexSize);
       gl_FragColor = texColor;
     }
   `
@@ -36,12 +37,14 @@ export default class TextureShader extends DefaultShader {
   private readonly vertexUVLoc: GLuint | null;
 
   private readonly texLoc: WebGLUniformLocation | null;
+  private readonly texSizeLoc: WebGLUniformLocation | null;
 
   public constructor(gl: WebGLRenderingContext, shaderSource?: ShaderSource) {
     super(gl, shaderSource || TEXTURE_SHADER_SOURCE);
 
     this.vertexUVLoc = this.getAttribLocation(gl, 'aVertexUV');
     this.texLoc = this.getUniformLocation(gl, 'uTex');
+    this.texSizeLoc = this.getUniformLocation(gl, 'uTexSize');
   }
 
   public enableVertexUVPosition(
@@ -52,16 +55,18 @@ export default class TextureShader extends DefaultShader {
     stride: number,
     offset: number
   ): void {
-    if (this.vertexUVLoc != null) {
+    if (this.vertexUVLoc != null && this.vertexUVLoc >= 0) {
       gl.vertexAttribPointer(this.vertexUVLoc, components, type, normalize, stride, offset);
       gl.enableVertexAttribArray(this.vertexUVLoc);
     }
   }
 
-  public setTexture(gl: WebGLRenderingContext, tex: Texture, texPos?: number): void {
+  public setTexture(gl: WebGLRenderingContext, tex: Texture, texSize?: number, texPos?: number): void {
     gl.activeTexture(gl.TEXTURE0 + (texPos || 0));
     gl.bindTexture(gl.TEXTURE_2D, tex);
 
     this.setUniform1i(gl, this.texLoc, texPos || 0);
+    this.setUniform1f(gl, this.texSizeLoc, texSize || 1.0);
   }
+
 }
